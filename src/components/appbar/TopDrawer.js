f5a178ef977e11b3b7801e42dd511b9016818426
nikowis <nikowis@gmail.com>
2020-05-18 23:52:36 +0200

    Add button to top drawer

--- src/components/appbar/TopDrawer.js
+++ src/components/appbar/TopDrawer.js
@@ -6,10 +6,17 @@ import MenuIcon from '@material-ui/icons/Menu';
 import Drawer from "@material-ui/core/Drawer";
 import List from "@material-ui/core/List";
 import TopMenuListItems from "./TopMenuListItems";
+import Divider from "@material-ui/core/Divider";
+import Button from "@material-ui/core/Button";
+import {REGISTER} from "../../common/paths";
+import {useTranslation} from "react-i18next";
+import ListItem from "@material-ui/core/ListItem";
+import VpnKeyIcon from '@material-ui/icons/VpnKey';
 
 function TopDrawer(props) {
 
     const [open, setOpen] = React.useState(false);
+    const {t} = useTranslation();
 
     const toggleDrawer = (open) => (event) => {
         if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
@@ -20,6 +27,11 @@ function TopDrawer(props) {
 
     const handleClose = (event) => {
         toggleDrawer(false)(event);
+    };
+
+    const redirect = (to) => {
+        setOpen(false);
+        props.history.push(to);
     };
 
     return (
@@ -37,6 +49,17 @@ function TopDrawer(props) {
                     onClick={toggleDrawer(false)}
                     onKeyDown={toggleDrawer(false)}
                 >
+                    {!props.authenticated ?
+                        <List>
+                            <ListItem>
+                                <Button size={"small"} variant="outlined" onClick={() => redirect(REGISTER)}
+                                        color="inherit" startIcon={<VpnKeyIcon/>}>
+                                    {t('register.page')}
+                                </Button>
+                            </ListItem>
+                        </List> : null
+                    }
+                    <Divider/>
                     <List>
                         <TopMenuListItems authenticated={props.authenticated} handleClose={handleClose}/>
                     </List>
