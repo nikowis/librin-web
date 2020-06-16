ad8cf25db86d53293392220e080b8c927296bd7c
nikowis <nikowis@gmail.com>
2020-06-16 20:58:18 +0200

    Rework messages buttons.

--- src/components/messages/ConversationsView.js
+++ src/components/messages/ConversationsView.js
@@ -8,12 +8,12 @@ import PropTypes from "prop-types";
 import List from "@material-ui/core/List";
 import LoaderComponent from "../LoaderComponent";
 import {useTranslation} from "react-i18next";
-import KeyboardArrowDownIcon from '@material-ui/icons/KeyboardArrowDown';
-import KeyboardArrowUpIcon from '@material-ui/icons/KeyboardArrowUp';
+import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
+import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
 import ListSubheader from "@material-ui/core/ListSubheader";
-import Fab from "@material-ui/core/Fab";
 import MaxWidthContainer from "../MaxWidthContainer";
 import {READ_CONVERSATION} from "../../redux/actions";
+import Divider from "@material-ui/core/Divider";
 
 function ConversationsView(props) {
 
@@ -35,25 +35,32 @@ function ConversationsView(props) {
     };
 
     const handleMarkConversationAsRead = (conversation) => {
-        if(!conversation.read) {
+        if (!conversation.read) {
             Api.markConversationAsRead(conversation.id);
             dispatch({type: READ_CONVERSATION, payload: {id: conversation.id}})
         }
     };
 
-    let prevConversationLoader = currentPage > 1 ? (
-        <Fab variant="extended" onClick={() => loadPrevConversations()}>
-            <KeyboardArrowUpIcon/>
+    let prevConversationLoader = currentPage > 1 ?
+        <div className={'MuiButtonBase-root button'} onClick={() => loadPrevConversations()}>
+            <ArrowBackIosIcon/>
             {t('messages.newer')}
-        </Fab>
-    ) : null;
+        </div> :
+        <div className={'MuiButtonBase-root button disabled'}>
+            <ArrowBackIosIcon/>
+            {t('messages.newer')}
+        </div>;
 
-    let nextConversationLoader = currentPage >= totalPages ? null : (
-        <Fab variant="extended" onClick={() => loadNextConversations()}>
-            <KeyboardArrowDownIcon/>
+    let nextConversationLoader = currentPage >= totalPages ?
+        <div className={'MuiButtonBase-root button disabled'}>
             {t('messages.older')}
-        </Fab>
-    );
+            <ArrowForwardIosIcon/>
+        </div> :
+        <div className={'MuiButtonBase-root button'} onClick={() => loadNextConversations()}>
+            {t('messages.older')}
+            <ArrowForwardIosIcon/>
+        </div>
+    ;
 
     return (
         <MaxWidthContainer size={'sm'}>
@@ -61,9 +68,14 @@ function ConversationsView(props) {
                 {
                     conversations && !mustReload ?
                         <List subheader={<ListSubheader>{t('messages.conversationsList')}</ListSubheader>}>
-                            {prevConversationLoader}
-                            <ConversationsList userId={userId} conversations={conversations} onConversationOpen={handleMarkConversationAsRead}/>
-                            {nextConversationLoader}
+                            <ConversationsList userId={userId} conversations={conversations}
+                                               onConversationOpen={handleMarkConversationAsRead}/>
+                            <Divider variant="fullWidth"/>
+                            <div className={'navigation-buttons'}>
+
+                                {prevConversationLoader}
+                                {nextConversationLoader}
+                            </div>
                         </List>
                         : <LoaderComponent/>
                 }
