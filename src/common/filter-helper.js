2f9054a3e95b5d93db4e6ccca5e3d8e8db3567bb
nikowis <nikowis@gmail.com>
2020-11-29 18:05:21 +0100

    Fix offers filter and search algorithm

--- src/common/filter-helper.js
+++ src/common/filter-helper.js
@@ -2,7 +2,7 @@
 export function filterMatchesUrl(filter, urlSearch) {
   const params = new URLSearchParams(urlSearch);
   for (const [key, value] of params) {
-      if(!filter[key] || String(filter[key]) !== value.toUpperCase()) {
+      if(!filter[key] || String(filter[key]).toLowerCase() !== value.toLowerCase()) {
         return false;
       }
   }
